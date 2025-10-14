d066ba3b34a2ba03504705790c5c810f24b806b4
kommit <96203792+kommittt@users.noreply.github.com>
2025-10-14 23:34:23 +0200

    Update example-server.py

--- example-server.py
+++ example-server.py
@@ -22,61 +22,140 @@ last_websocket_update: Dict[WebSocket, datetime] = {}  # rate-limiting (optional
 html = """
 <!DOCTYPE html>
 <html>
-  <head>
-    <title>Not-So-Global-CapsLock</title>
-    <style>
-      body {
-        font-family: sans-serif;
-        text-align: center;
-        margin-top: 50px;
-      }
-      .caps-state {
-        font-size: 2em;
-        margin: 20px;
-        color: green;
-      }
-      .caps-state.off {
-        color: red;
-      }
-      .clients-count {
-        font-size: 1.2em;
-      }
-    </style>
-  </head>
-  <body>
-    <h1>Not-So-Global-CapsLock</h1>
-    <div id="capsState" class="caps-state off">Caps Lock is OFF</div>
-    <div id="clientCount" class="clients-count">Connected: 0</div>
 
-    <script>
-      const capsDiv = document.getElementById("capsState");
-      const clientsDiv = document.getElementById("clientCount");
+    <head>
+        <title>not so global capslock</title>
 
-      // Connect to status websocket
-      const ws = new WebSocket(`wss://${window.location.host}/status`);
+        <link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,100..800;1,100..800&display=swap" rel="stylesheet">
 
-      ws.onmessage = (event) => {
-        const data = event.data;
+        <style>
+            html{
+                font-family: 'JetBrains Mono', sans-serif;
+                background-color: rgb(41, 41, 46);
+                color: rgb(215, 225, 233);
+                overflow: hidden;
+            }
 
-        if (data.startsWith("c ")) {
-          // Update number of connected clients
-          const count = data.split(" ")[1];
-          clientsDiv.textContent = `Connected: ${count}`;
-        } else if (data === "1") {
-          capsDiv.textContent = "Caps Lock is ON";
-          capsDiv.classList.remove("off");
-          capsDiv.classList.add("on");
-        } else if (data === "0") {
-          capsDiv.textContent = "Caps Lock is OFF";
-          capsDiv.classList.remove("on");
-          capsDiv.classList.add("off");
-        }
-      };
+            h1 {
+                margin-bottom: 3rem;
+                text-shadow: 0px 0px 5px rgba(255, 255, 255, 0.44);
+            }
 
-      ws.onopen = () => console.log("Status WebSocket connected");
-      ws.onclose = () => console.log("Status WebSocket disconnected");
+            body {
+                text-align: center;
+                margin-top: 50px;
+            }
+
+            .container{
+                border: solid;
+                border-radius: 10px;
+                border-color: rgb(41, 14, 14);
+                display: inline-block;
+                background-color: rgb(60, 24, 24);
+            }
+            .container.on{
+                border: solid;
+                border-radius: 10px;
+                border-color: rgb(14, 41, 16);
+                display: inline-block;
+                background-color: rgb(28, 60, 24);
+            }
+
+            .caps-state {
+                font-size: 2em;
+                margin: 20px;
+                color: green;
+            }
+            .caps-state.off {
+                color: red;
+            }
+
+            .clients-count {
+                margin-top: 2rem;
+                font-size: 1.2em;
+            }
+
+            .yap{
+                margin-top: 5rem;
+                bottom: 10px;
+                right: 10px;
+                font-size: 0.8em;
+            }
+
+            a{
+                text-decoration: none;
+                color: rgb(215, 225, 233);
+            }
+            a:hover{
+                text-decoration: underline;
+                color: rgb(0, 140, 255);
+                text-shadow: 0px 0px 5px rgba(0, 213, 255, 0.44);
+            }
+      </style>
+      </head>
+
+      <body>
+            <h1><a href="https://github.com/kommittt/not-so-global-capslock">not-so-global capslock</a></h1>
+
+            <div class="container">
+                <div id="capsState" class="caps-state off">caps lock is off</div>
+            </div>
+            
+            <div id="clientCount" class="clients-count">there are 0 people currently syncing</div>
+
+            <div class="yap">
+                how this works: <br>
+                everyone who is connected to the server will have their caps lock state synchronized <br>
+                so if someone turns on their caps, everyone else's caps will turn on too <br>
+                this can cause some chaos especially if there are a lot of people connected
+
+                <br><br><br>
+                <b>how to connect?</b> <br>
+                1. <a href="https://raw.githubusercontent.com/kommittt/not-so-global-capslock/main/client.py" download="client.py">click here to download the client.py file if you haven't</a> <br>
+                2. make sure you have python 3.10+ installed <br>
+                3. open command prompt, then type in: pip install websockets <br>
+                4. go to wherever you downloaded client.py <br>
+                5. then open terminal in that location and run it by typing: python client.py <br>
+
+                <br> <br>
+                <a href="https://globalcapslock.com/">check out the original project here</a>
+            </div>
+
+            <script>
+            const capsDiv = document.getElementById("capsState");
+            const clientsDiv = document.getElementById("clientCount");
+            const box = document.getElementById("container");
+
+            // Connect to status websocket
+            const ws = new WebSocket(`wss://${window.location.host}/status`);
+
+            ws.onmessage = (event) => {
+                const data = event.data;
+
+              if (data.startsWith("c ")) {
+                    // Update number of connected clients
+                    const count = data.split(" ")[1];
+                    clientsDiv.textContent = `there are ${count} people currently syncing`;
+              } else if (data === "1") {
+                    capsDiv.textContent = "CAPS LOCK IS ON";
+                    capsDiv.classList.remove("off");
+                    capsDiv.classList.add("on");
+                    box.classList.remove("off");
+                    box.classList.add("on");
+              } else if (data === "0") {
+                    capsDiv.textContent = "caps Lock is off";
+                    capsDiv.classList.remove("on");
+                    capsDiv.classList.add("off");
+                    box.classList.remove("on");
+                    box.classList.add("off");
+                }
+            };
+
+            ws.onopen = () => console.log("Status WebSocket connected");
+            ws.onclose = () => console.log("Status WebSocket disconnected");
     </script>
-  </body>
+    </body>
+
 </html>
 """
 
